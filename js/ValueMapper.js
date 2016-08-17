eff4c87d0441b720e46559bf9f0608474f454455
aleclarsoniv <alec.stanford.larson@gmail.com>
2016-08-17 16:47:07 -0700

    1.0.0

---
+++ js/ValueMapper.js
@@ -0,0 +1,101 @@
+var NamedFunction, Typle, ValueMapper, Void, assertType, assertTypes, configTypes, defineAll, defineEach, frozen, hidden, isType, reactive, ref, setType;
+
+ref = require("Property"), frozen = ref.frozen, hidden = ref.hidden, reactive = ref.reactive;
+
+NamedFunction = require("NamedFunction");
+
+assertTypes = require("assertTypes");
+
+assertType = require("assertType");
+
+setType = require("setType");
+
+isType = require("isType");
+
+Typle = require("Typle");
+
+Void = require("Void");
+
+configTypes = {
+  values: Typle([Object, Function]),
+  define: Typle([Function, Void])
+};
+
+ValueMapper = NamedFunction("ValueMapper", function(config) {
+  var key, preset, ref1, self;
+  assertTypes(config, configTypes);
+  if (!isType(config.define, Function)) {
+    ref1 = ValueMapper.presets;
+    for (key in ref1) {
+      preset = ref1[key];
+      if (config[key] === true) {
+        config.define = preset;
+        break;
+      }
+    }
+  }
+  self = {
+    _values: config.values,
+    _define: config.define
+  };
+  if (isType(config.values, Function)) {
+    self.define = defineAll;
+  } else {
+    self.define = defineEach;
+  }
+  return setType(self, ValueMapper);
+});
+
+module.exports = ValueMapper;
+
+ValueMapper.presets = {
+  mutable: function(obj, key, value) {
+    if (value !== void 0) {
+      obj[key] = value;
+    }
+  },
+  frozen: function(obj, key, value) {
+    if (value !== void 0) {
+      frozen.define(obj, key, {
+        value: value
+      });
+    }
+  },
+  reactive: function(obj, key, value) {
+    if (value !== void 0) {
+      reactive.define(obj, key, {
+        value: value
+      });
+    }
+  }
+};
+
+defineAll = function(obj, args) {
+  var key, value, values;
+  values = this._values.apply(obj, args);
+  if (!isType(values, Object)) {
+    throw TypeError("Must return an Object!");
+  }
+  for (key in values) {
+    value = values[key];
+    this._define(obj, key, value);
+  }
+};
+
+defineEach = function(obj, args) {
+  var key, ref1, value;
+  ref1 = this._values;
+  for (key in ref1) {
+    value = ref1[key];
+    if (isType(value, Function)) {
+      if (value.length) {
+        value = value.apply(obj, args);
+      } else {
+        value = value.call(obj);
+      }
+    }
+    this._define(obj, key, value);
+  }
+};
+
+//# sourceMappingURL=map/ValueMapper.map
---
+++ src/ValueMapper.coffee
@@ -0,0 +1,73 @@
+
+{frozen, hidden, reactive} = require "Property"
+
+NamedFunction = require "NamedFunction"
+assertTypes = require "assertTypes"
+assertType = require "assertType"
+setType = require "setType"
+isType = require "isType"
+Typle = require "Typle"
+Void = require "Void"
+
+configTypes =
+  values: Typle [ Object, Function ]
+  define: Typle [ Function, Void ]
+
+ValueMapper = NamedFunction "ValueMapper", (config) ->
+  assertTypes config, configTypes
+
+  unless isType config.define, Function
+    for key, preset of ValueMapper.presets
+      if config[key] is yes
+        config.define = preset
+        break
+
+  self =
+    _values: config.values
+    _define: config.define
+
+  if isType config.values, Function
+    self.define = defineAll
+  else self.define = defineEach
+
+  return setType self, ValueMapper
+
+module.exports = ValueMapper
+
+ValueMapper.presets =
+
+  mutable: (obj, key, value) ->
+    if value isnt undefined
+      obj[key] = value
+    return
+
+  frozen: (obj, key, value) ->
+    if value isnt undefined
+      frozen.define obj, key, { value }
+    return
+
+  reactive: (obj, key, value) ->
+    if value isnt undefined
+      reactive.define obj, key, { value }
+    return
+
+#
+# Helpers
+#
+
+defineAll = (obj, args) ->
+  values = @_values.apply obj, args
+  if not isType values, Object
+    throw TypeError "Must return an Object!"
+  for key, value of values
+    @_define obj, key, value
+  return
+
+defineEach = (obj, args) ->
+  for key, value of @_values
+    if isType value, Function
+      if value.length
+        value = value.apply obj, args
+      else value = value.call obj
+    @_define obj, key, value
+  return
